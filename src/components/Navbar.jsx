c8729bed4e8bb2a529ef44a5b15f68f75e109899
Rintia Islam <rintiaislam110@gmail.com>
2026-05-18 22:56:55 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
+import Image from 'next/image';
 import React from 'react';
-
+import logo from '../assets/logo.jpg'
 const Navbar = () => {
     return (
         <div className="navbar bg-base-100 shadow-sm">
@@ -11,36 +12,28 @@ const Navbar = () => {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
+        <li><a>Home</a></li>
         <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
+          <a>All Pets</a>
         </li>
-        <li><a>Item 3</a></li>
+        <li><a>My Requests</a></li>
+        <li><a>Add Pet</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl"><Image src={logo} height={150} width={90}></Image></a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <li><a>Home</a></li>
+        <li>
+          <a>All Pets</a>
+        </li>
+        <li><a>My Requests</a></li>
+        <li><a>Add Pet</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn">Login</a>
   </div>
 </div>
     );
